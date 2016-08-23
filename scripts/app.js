b723b3b4594b7b8585ca64ef137b076b825d4843
wnamen <williamjosephnamen@gmail.com>
2016-08-23 15:55:39 -0700

    upated the forms and created ajax objects

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,39 @@
 $(document).on("ready", function(){
 
+  $.ajax({
+    method: "GET",
+    url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+    dataType: "json",
+    success: onSuccess,
+    error: onError
+  })
+
+  $("btn").click(function clickSearch(event) {
+    $.ajax({
+      method: "GET",
+      url: "http://api.giphy.com/v1/gifs/search?",
+      data: $("form").serialize(),
+      dataType: "json",
+      success: onSuccess,
+      error: onError
+    })
+  })
+
+  function onSuccess(json) {
+    var jData = json.data;
+
+    for (var i = 0; i < jData.length; i++) {
+      var img = jData[i].images.fixed_height.url;
+
+      $("div").append("<img src=" + img +">");
+    }
+  }
+
+  function onError(xhr, status, errorThrown) {
+      alert("Sorry, there was a problem!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
+  }
+
 });
