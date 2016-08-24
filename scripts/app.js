c1a12b7d1b60bfeb3788c9f01b785732450b4278
wnamen <williamjosephnamen@gmail.com>
2016-08-23 17:19:04 -0700

    finsished search query logic and added styling

--- scripts/app.js
+++ scripts/app.js
@@ -8,10 +8,14 @@ $(document).on("ready", function(){
     error: onError
   })
 
-  $("btn").click(function clickSearch(event) {
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+
+    $(".gif-gallery").empty();
+
     $.ajax({
       method: "GET",
-      url: "http://api.giphy.com/v1/gifs/search?",
+      url: "http://api.giphy.com/v1/gifs/search",
       data: $("form").serialize(),
       dataType: "json",
       success: onSuccess,
@@ -20,12 +24,14 @@ $(document).on("ready", function(){
   })
 
   function onSuccess(json) {
+
+    console.log("success");
     var jData = json.data;
 
     for (var i = 0; i < jData.length; i++) {
       var img = jData[i].images.fixed_height.url;
 
-      $("div").append("<img src=" + img +">");
+      $(".gif-gallery").append("<img src=" + img +">");
     }
   }
 
